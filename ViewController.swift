b8fd0f0a0d3448d6b1e6e411585a2272b5428dbf
Zeitlin1 <Zeitlin1@gmail.com>
2016-09-27 18:41:33 -0400

    Add files via upload
    add UI Buttons and code.

---
+++ ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  UIFun
+//
+//  Created by Jim Campagno on 9/17/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // TODO: Setup the IBOutlets
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBOutlet weak var paintBucket: UIImageView!
+    
+    @IBOutlet weak var firstColorSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var secondColorSegmentedControl: UISegmentedControl!
+    
+    
+    func mixColors(first: String, second: String) -> UIColor {
+        
+        switch (first, second) {
+            
+        case ("Red", "Red"):
+            return UIColor.red
+        case ("Red", "Yellow"), ("Yellow", "Red"):
+            return UIColor.orange
+        case ("Red", "Blue"), ("Blue", "Red"):
+            return UIColor.purple
+        case ("Yellow", "Yellow"):
+            return UIColor.yellow
+        case ("Yellow", "Blue"), ("Blue", "Yellow"):
+            return UIColor.green
+        case ("Blue", "Blue"):
+            return UIColor.blue
+        default:
+            return UIColor.white
+        }
+        
+    }
+    
+    @IBAction func colorSelected(_ sender: UISegmentedControl) {
+        
+        let firstSelection = firstColorSegmentedControl.selectedSegmentIndex
+        let secondSelection = secondColorSegmentedControl.selectedSegmentIndex
+        
+        
+        switch (firstSelection, secondSelection) {
+            
+        case (0, 0):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Red")
+        
+        case (0, 1):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Yellow")
+            
+        case (1, 0):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Red")
+        
+        case (0, 2):
+            paintBucket.backgroundColor = mixColors(first: "Red", second: "Blue")
+            
+        case (2, 0):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Red")
+        
+        case (1, 1):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Yellow")
+        
+        case (1, 2):
+            paintBucket.backgroundColor = mixColors(first: "Yellow", second: "Blue")
+            
+        case (2, 1):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Yellow")
+        
+        case (2, 2):
+            paintBucket.backgroundColor = mixColors(first: "Blue", second: "Blue")
+        
+        default :
+            paintBucket.backgroundColor = UIColor.white
+            
+        
+        }
+        // TODO: Implement this function according to the instructions.
+        
+        
+    }
+
+}
